89b4481544e437d4c6b497845cfe2eac088fa451
Raymond Schweers <raymond@hetwebbureau.nl>
2021-07-31 21:19:13 +0200

    adding -y on apt installs

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository -y --update ppa:ansible/ansible
-sudo apt -y install ansible
+sudo apt -y update
+sudo apt -y install software-properties-common
+sudo add-apt-repository --update ppa:ansible/ansible
+sudo apt -y --force-yes install ansible
 
 sudo ansible-pull -U https://github.com/R4YM3/dotfiles-ansible.git
