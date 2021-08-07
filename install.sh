d51c744f347b2c452011332483468e0cbc7161f8
Raymond Schweers <raymond@hetwebbureau.nl>
2021-08-07 23:45:04 +0200

    removed sudo

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# IF MAC...
+# xcode-select --install
+
 sudo apt -y update
 sudo apt -y install software-properties-common
 sudo add-apt-repository --update ppa:ansible/ansible
 sudo apt -y --force-yes install ansible
 
-sudo ansible-pull -U https://github.com/R4YM3/dotfiles-ansible.git
+ansible-pull -U https://github.com/R4YM3/dotfiles-ansible.git
