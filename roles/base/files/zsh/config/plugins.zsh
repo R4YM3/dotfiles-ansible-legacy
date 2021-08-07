87f506b2cc333260097aa58cdc0258cd7a1d01c3
Raymond Schweers <raymond@hetwebbureau.nl>
2021-08-07 21:30:31 +0200

    zsh: install zsh-nvm

--- roles/base/files/zsh/config/plugins.zsh
+++ roles/base/files/zsh/config/plugins.zsh
@@ -2,8 +2,4 @@
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(aws git nvm zsh-syntax-highlighting zsh-autosuggestions)
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+plugins=(git zsh-nvm zsh-syntax-highlighting zsh-autosuggestions aws)
