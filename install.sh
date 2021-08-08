b0ccf187ed6b4df9f1eeea9c281c5e5858eea6b5
Raymond Schweers <raymond@hetwebbureau.nl>
2021-08-08 20:34:21 +0200

    fix broken install

--- install.sh
+++ install.sh
@@ -8,18 +8,17 @@ if [ -f "/etc/debian_version" ]; then
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
-    sudo apt -y --fix-broken install
+    sudo apt --fix-broken install -y
 
-    sudo apt -y update
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
-    sudo apt-get full-upgrade -y
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt full-upgrade -y
 
-    sudo apt -y install software-properties-common
-    sudo apt -y install python3
+    sudo apt install software-properties-common -y
+    sudo apt install python3 -y
 
     sudo add-apt-repository --update ppa:ansible/ansible
-    sudo apt -y --force-yes install ansible
+    sudo apt --force-yes install ansible -y
 
     ansible-pull -U https://github.com/R4YM3/dotfiles-ansible.git --ask-become-pass
   fi
